29189399964f68a47a53b010201ba6f46b2ffa33
Pritam <pri10145@gmail.com>
2023-05-04 09:09:52 +0530

    Hero Section Completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,6 +18,22 @@ const Hero = () => {
     },
   ]);
 
+  const [openOptions, setOpenOptions] = useState(false);
+  const [options, setOptions] = useState({
+    adult: 1,
+    minor: 0,
+  });
+
+  
+  const handleOptions = (name, oparetion) => {
+    setOptions((prev) => {
+      return {
+        ...prev,
+        [name]: oparetion === "i" ? options[name] + 1 : options[name] - 1,
+      };
+    });
+  };
+
   return (
     <>
       <header className="flex flex-col items-center relative w-full h-[529px] px-7 py-4">
@@ -26,7 +42,7 @@ const Hero = () => {
             It's more than <br /> just a trip
           </h1>
         </div>
-    
+
         <div className="flex w-full max-w-[1024px] lg:h-[65px] lg:flex-row items-center flex-col mt-20  shadowCard relative ">
           <div className="flex w-full h-full justify-start items-center border-[1px] border-[#CBD4E6] p-2 lg:rounded-l-[4px]">
             <img src={departure} alt="departure" />
@@ -49,15 +65,15 @@ const Hero = () => {
           <div className="flex w-full h-full justify-start items-center border-[1px] border-[#CBD4E6] p-2">
             <img src={calendar} alt="calendar" />
             <span
-              className='text-[#7C8DB0] text-base leading-6 ml-2 cursor-pointer'
+              className="text-[#7C8DB0] text-base leading-6 ml-2 cursor-pointer"
               onClick={() => setOpenDate(!openDate)}
             >
-           {openDate ? (
-               `${format(date[0].startDate, "dd/MM/yyyy")} to ${format(
-                date[0].endDate,
-                "dd/MM/yyyy"
-              )}`
-           ) : 'Depart to Return'}
+              {openDate
+                ? `${format(date[0].startDate, "dd/MM/yyyy")} to ${format(
+                    date[0].endDate,
+                    "dd/MM/yyyy"
+                  )}`
+                : "Depart to Return"}
             </span>
             {openDate && (
               <DateRange
@@ -72,22 +88,57 @@ const Hero = () => {
 
           <div className="flex w-full h-full justify-start items-center border-[1px] border-[#CBD4E6]  p-2">
             <img src={person} alt="person" />
-            <span className="text-[#7C8DB0] text-base leading-6 ml-2 cursor-pointer">
-              1 adult
+            <span
+              className="text-[#7C8DB0] text-base leading-6 ml-2 cursor-pointer"
+              onClick={() => setOpenOptions(!openOptions)}
+            >
+              {`${options.adult} Adult - ${options.minor} Minor `}
             </span>
+            {openOptions && (
+              <div className="w-52 h-fit flex flex-col gap-4 rounded-md bg-white shadowCard absolute lg:top-[70px] top-64 p-4 z-10">
+                <div className="flex justify-between items-center">
+                  <span className="text-[#7C8DB0] text-base leading-6">
+                    Adults:
+                  </span>
+                  <div className="flex items-center gap-4">
+                    <button className="border-2 border-[#605DEC] px-2 text-[#7C8DB0] disabled:cursor-not-allowed" onClick={() =>handleOptions("adult", "d")} disabled={options.adult <= 1}>
+                      -
+                    </button>
+                    <span className="text-[#7C8DB0]">{options.adult}</span>
+                    <button className="border-2 border-[#605DEC] px-2 text-[#7C8DB0]" onClick={()=>handleOptions("adult", "i")}>
+                      +
+                    </button>
+                  </div>
+                </div>
+                <div className="flex justify-between items-center">
+                  <span className="text-[#7C8DB0] text-base leading-6">
+                    Minors:
+                  </span>
+                  <div className="flex items-center gap-4">
+                    <button className="border-2 border-[#605DEC] px-2 text-[#7C8DB0] disabled:cursor-not-allowed" onClick={()=> handleOptions("minor", "d")} disabled={options.minor <= 0}>
+                      -
+                    </button>
+                    <span className="text-[#7C8DB0]">{options.minor}</span>
+                    <button className="border-2 border-[#605DEC] px-2 text-[#7C8DB0]" onClick={()=> handleOptions("minor", "i")}>
+                      +
+                    </button>
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
+
           <div className="w-full ">
-          <button className="w-full bg-[#605DEC] text-[#FAFAFA] text-lg leading-6 h-[45px] lg:h-[65px] px-5   lg:rounded-r-[4px]">
-            Search
-          </button>
+            <button className="w-full bg-[#605DEC] text-[#FAFAFA] text-lg leading-6 h-[45px] lg:h-[65px] px-5   lg:rounded-r-[4px]">
+              Search
+            </button>
+          </div>
         </div>
-        </div>
-
-       
-
       </header>
     </>
   );
 };
 
 export default Hero;
+
+//git push -u origin main

5850f8eccc013342027c2daeee95cd3da4e85c68
Pritam <pri10145@gmail.com>
2023-05-06 09:41:28 +0530

    Passenger info updated

--- src/pages/PassengerInfo.jsx
+++ src/pages/PassengerInfo.jsx
@@ -1,9 +1,145 @@
-
-
 const PassengerInfo = () => {
   return (
-    <div>PassengerInfo</div>
-  )
-}
+    <>
+      <div className="px-8 w-full flex lg:flex-row flex-col mt-20">
+        <div className="w-full lg:w-[682px] flex flex-col items-start gap-10">
+          <div className="flex flex-col items-start gap-2 w-full">
+            <h1 className="text-[#605DEC] text-xl font-bold">
+              Passenger information
+            </h1>
+            <p className="text-[#7C8DB0] text-base font-normal">
+              Enter the required information for each traveler and be sure that
+              it exactly matches the government-issued ID presented at the
+              airport.
+            </p>
+          </div>
+          <div className="flex flex-col items-start w-full gap-4">
+            <h2 className="text-[#6E7491] text-lg font-medium">
+              Passenger 1 (Adult)
+            </h2>
+            <form className="flex flex-col w-full md:flex-row items-center justify-start gap-4">
+              <input
+                type="text"
+                placeholder="First Name*"
+                className=" w-full border-[1px] border-[#A1B0CC] outline-none px-2 py-3 text-[#7C8DB0] placeholder:text-[#7C8DB0] rounded"
+              />
+              <input
+                type="text"
+                placeholder="Middle Name*"
+                className="w-full border-[1px] border-[#A1B0CC] outline-none px-2 py-3 text-[#7C8DB0] placeholder:text-[#7C8DB0] rounded"
+              />
+              <input
+                type="text"
+                placeholder="Last Name*"
+                className=" w-full border-[1px] border-[#A1B0CC] outline-none px-2 py-3 text-[#7C8DB0] placeholder:text-[#7C8DB0] rounded"
+              />
+            </form>
+            <form className="flex flex-col w-full md:flex-row items-center justify-start gap-4">
+              <input
+                type="text"
+                placeholder="Suffix"
+                className=" w-full border-[1px] border-[#A1B0CC] outline-none px-1 py-3 text-[#7C8DB0] placeholder:text-[#7C8DB0] rounded"
+              />
+              <input
+                type="text"
+                placeholder="Date of birth*"
+                className="w-full border-[1px] border-[#A1B0CC] outline-none px-3 py-3 text-[#7C8DB0] placeholder:text-[#7C8DB0] rounded"
+              />
+            </form>
+            <form className="flex flex-col w-full md:flex-row items-center justify-start gap-4 mt-2">
+              <input
+                type="email"
+                placeholder="Email Adress*"
+                className="w-full border-[1px] border-[#A1B0CC] outline-none px-2 py-3 text-[#7C8DB0] placeholder:text-[#7C8DB0] rounded"
+              />
+              <input
+                type="text"
+                placeholder="Phone Number*"
+                className="w-full border-[1px] border-[#A1B0CC] outline-none px-2 py-3 text-[#7C8DB0] placeholder:text-[#7C8DB0] rounded"
+              />
+            </form>
+            <form className="flex flex-col w-full md:flex-row items-center justify-start gap-4">
+              <input
+                type="text"
+                placeholder="Redress number"
+                className="w-full border-[1px] border-[#A1B0CC] outline-none px-2 py-3 text-[#7C8DB0] placeholder:text-[#7C8DB0] rounded"
+              />
+              <input
+                type="text"
+                placeholder="Known traveller number"
+                className="w-full border-[1px] border-[#A1B0CC] outline-none px-2 py-3 text-[#7C8DB0] placeholder:text-[#7C8DB0] rounded"
+              />
+            </form>
+          </div>
+          <div className="flex flex-col items-start w-full gap-4">
+            <h2 className="text-[#6E7491] text-lg font-medium">
+              Emergency contact information
+            </h2>
+            <div className="flex items-center justify-start gap-2 mt-2">
+              <input type="checkbox" name="checkbox" id="checkbox" />
+              <label htmlFor="checkbox" className="text-[#6E7491] font-normal">
+                Same as Passenger 1
+              </label>
+            </div>
+            <form className="flex flex-col w-full md:flex-row items-center justify-start gap-4 mt-4">
+              <input
+                type="text"
+                placeholder="First Name*"
+                className=" w-full border-[1px] border-[#A1B0CC] outline-none px-2 py-3 text-[#7C8DB0] placeholder:text-[#7C8DB0] rounded"
+              />
+              <input
+                type="text"
+                placeholder="Middle Name*"
+                className="w-full border-[1px] border-[#A1B0CC] outline-none px-2 py-3 text-[#7C8DB0] placeholder:text-[#7C8DB0] rounded"
+              />
+            </form>
+            <form className="flex flex-col w-full md:flex-row items-center justify-start gap-4 mt-2">
+              <input
+                type="email"
+                placeholder="Email Adress*"
+                className="w-full border-[1px] border-[#A1B0CC] outline-none px-2 py-3 text-[#7C8DB0] placeholder:text-[#7C8DB0] rounded"
+              />
+              <input
+                type="text"
+                placeholder="Phone Number*"
+                className="w-full border-[1px] border-[#A1B0CC] outline-none px-2 py-3 text-[#7C8DB0] placeholder:text-[#7C8DB0] rounded"
+              />
+            </form>
+          </div>
+          <div className="flex flex-col items-start gap-2 w-full">
+            <h1 className="text-[#605DEC] text-xl font-bold">
+            Bag information
+            </h1>
+            <p className="text-[#7C8DB0] text-base font-normal">
+            Each passenger is allowed one free carry-on bag and one personal item. First checked bag for each passenger is also free. Second bag check fees are waived for loyalty program members. See the <span className="text-[#605DEC]"> full bag policy.</span>
+            </p>
+          </div>
+          <div className="w-full sm:w-[400px] flex items-start justify-between gap-10">
+              <div className="flex flex-col items-start gap-2">
+                  <p className="text-[#7C8DB0] text-base font-semibold">Passenger 1</p>
+                  <p className="text-[#6E7491] text-base font-semibold">First Last</p>
+              </div>
+              <div className="flex flex-col items-center gap-2">
+                  <p className="text-[#7C8DB0] text-base font-semibold">Checked bags</p>
+                  <div className=" flex items-center gap-4">
+                    <span className="text-[#605DEC] text-3xl font-semibold cursor-pointer">-</span>
+                    <span className="text-[#6E7491] text-base font-semibold">1</span>
+                    <span className="text-[#605DEC] text-xl font-semibold cursor-pointer ">+</span>
+                  </div>
+              </div>
+          </div>
+          <div className="flex items-center gap-5">
+            <button className="py-2 px-4 border-[1px] border-[#605DEC] text-[#605DEC] rounded hover:bg-[#605DEC] hover:text-white transition-all duration-200">Save & close</button>
+            <button className="py-2 px-4 border-[1px] border-[#7C8DB0] text-[#7C8DB0] bg-[#CBD4E6] rounded hover:bg-white transition-all duration-200">Select seats</button>
+          </div>
+        </div>
 
-export default PassengerInfo
+        <div>
+          
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PassengerInfo;

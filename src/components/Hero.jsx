52291c9e69e531eca9ae451cd105c8ff7f0558e4
Pritam <pri10145@gmail.com>
2023-05-08 10:02:23 +0530

    Add some coustom search data in two inputs

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,6 +8,40 @@ import { format } from "date-fns";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
+import { suggestions } from "../data/constant";
+
+
+const AutoSuggest = (initialValue) => {
+  const [input, setInput] = useState('');
+  const [matchingSuggestions, setMatchingSuggestions] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleInputChange = (event) => {
+    const inputValue = event.target.value.toLowerCase();
+    setInput(inputValue);
+
+    const filteredSuggestions = suggestions.filter((suggestion) =>
+      suggestion.toLowerCase().startsWith(inputValue)
+    );
+    setMatchingSuggestions(filteredSuggestions);
+  };
+
+  const handleSuggestionClick = (suggestion) => {
+    setInput(suggestion);
+    setIsOpen(false);
+  };
+
+  return {
+    input,
+    matchingSuggestions,
+    isOpen,
+    setInput,
+    setIsOpen,
+    handleInputChange,
+    handleSuggestionClick,
+  };
+}
+
 const Hero = () => {
   // const navigate = useNavigate();
   const [openDate, setOpenDate] = useState(false);
@@ -25,7 +59,6 @@ const Hero = () => {
     minor: 0,
   });
 
-  
   const handleOptions = (name, oparetion) => {
     setOptions((prev) => {
       return {
@@ -34,6 +67,10 @@ const Hero = () => {
       };
     });
   };
+
+  const departureSuggest = AutoSuggest('');
+  const arrivalSuggest = AutoSuggest('');
+  
 
   return (
     <>
@@ -45,22 +82,55 @@ const Hero = () => {
         </div>
 
         <div className="flex w-full max-w-[1024px] lg:h-[65px] lg:flex-row items-center flex-col mt-20  shadowCard relative ">
-          <div className="flex w-full h-full justify-start items-center border-[1px] border-[#CBD4E6] p-2 lg:rounded-l-[4px]">
+          <div className="flex w-full h-full justify-start items-center border-[1px] border-[#CBD4E6] p-2 lg:rounded-l-[4px] relative">
             <img src={departure} alt="departure" />
             <input
               type="text"
               placeholder="From where?"
-              className="outline-none border-none ml-2 placeholder:text-[#7C8DB0] placeholder:text-base placeholder:leading-6"
+              value={departureSuggest.input}
+              onChange={departureSuggest.handleInputChange}
+              onFocus={() => departureSuggest.setIsOpen(true)}
+              className="uppercase placeholder:capitalize outline-none border-none ml-2 text-base text-[#7C8DB0] placeholder:text-[#7C8DB0] placeholder:text-base placeholder:leading-6"
+             
             />
+           { departureSuggest.isOpen && ( 
+           <ul className="w-[220px] h-56 absolute top-[70px]  bg-white rounded overflow-scroll">
+              {departureSuggest.matchingSuggestions.map((suggestion) => (
+                <li
+                  key={suggestion}
+                  onClick={() => departureSuggest.handleSuggestionClick(suggestion)}
+                  className="uppercase  cursor-pointer hover:bg-[#605DEC] px-3 py-1 text-[#7C8DB0] hover:text-[#F6F6FE]  mt-1"
+                >
+                  {suggestion}
+                </li>
+              ))}
+            </ul>
+            )}
           </div>
 
           <div className="flex w-full h-full justify-start items-center border-[1px] border-[#CBD4E6] p-2">
-            <img src={arrival} alt="departure" />
+            <img src={arrival} alt="arrival" />
             <input
               type="text"
               placeholder="Where to?"
-              className="outline-none border-none ml-2 placeholder:text-[#7C8DB0] placeholder:text-base placeholder:leading-6"
+              value={arrivalSuggest.input}
+        onChange={arrivalSuggest.handleInputChange}
+        onFocus={() => arrivalSuggest.setIsOpen(true)}
+              className="uppercase placeholder:capitalize outline-none border-none ml-2 text-base text-[#7C8DB0] placeholder:text-[#7C8DB0] placeholder:text-base placeholder:leading-6"
             />
+           { arrivalSuggest.isOpen && (
+            <ul className="w-[220px] h-56 absolute top-[70px] bg-white rounded overflow-scroll">
+              {arrivalSuggest.matchingSuggestions.map((suggestion) => (
+                <li
+                  key={suggestion}
+                  onClick={() => arrivalSuggest.handleSuggestionClick(suggestion)}
+                  className="uppercase cursor-pointer hover:bg-[#605DEC] px-3 py-1 text-[#7C8DB0] hover:text-[#F6F6FE]  mt-1"
+                >
+                  {suggestion}
+                </li>
+              ))}
+            </ul>
+            )}
           </div>
 
           <div className="flex w-full h-full justify-start items-center border-[1px] border-[#CBD4E6] p-2">
@@ -102,11 +172,18 @@ const Hero = () => {
                     Adults:
                   </span>
                   <div className="flex items-center gap-4">
-                    <button className="border-2 border-[#605DEC] px-2 text-[#7C8DB0] disabled:cursor-not-allowed" onClick={() =>handleOptions("adult", "d")} disabled={options.adult <= 1}>
+                    <button
+                      className="border-2 border-[#605DEC] px-2 text-[#7C8DB0] disabled:cursor-not-allowed"
+                      onClick={() => handleOptions("adult", "d")}
+                      disabled={options.adult <= 1}
+                    >
                       -
                     </button>
                     <span className="text-[#7C8DB0]">{options.adult}</span>
-                    <button className="border-2 border-[#605DEC] px-2 text-[#7C8DB0]" onClick={()=>handleOptions("adult", "i")}>
+                    <button
+                      className="border-2 border-[#605DEC] px-2 text-[#7C8DB0]"
+                      onClick={() => handleOptions("adult", "i")}
+                    >
                       +
                     </button>
                   </div>
@@ -116,11 +193,18 @@ const Hero = () => {
                     Minors:
                   </span>
                   <div className="flex items-center gap-4">
-                    <button className="border-2 border-[#605DEC] px-2 text-[#7C8DB0] disabled:cursor-not-allowed" onClick={()=> handleOptions("minor", "d")} disabled={options.minor <= 0}>
+                    <button
+                      className="border-2 border-[#605DEC] px-2 text-[#7C8DB0] disabled:cursor-not-allowed"
+                      onClick={() => handleOptions("minor", "d")}
+                      disabled={options.minor <= 0}
+                    >
                       -
                     </button>
                     <span className="text-[#7C8DB0]">{options.minor}</span>
-                    <button className="border-2 border-[#605DEC] px-2 text-[#7C8DB0]" onClick={()=> handleOptions("minor", "i")}>
+                    <button
+                      className="border-2 border-[#605DEC] px-2 text-[#7C8DB0]"
+                      onClick={() => handleOptions("minor", "i")}
+                    >
                       +
                     </button>
                   </div>
@@ -129,7 +213,7 @@ const Hero = () => {
             )}
           </div>
 
-          <Link to='/explore' className="w-full ">
+          <Link to="/explore" className="w-full ">
             <button className="w-full bg-[#605DEC] text-[#FAFAFA] text-lg leading-6 h-[45px] lg:h-[65px] px-5   lg:rounded-r-[4px]">
               Search
             </button>
---
+++ src/data/constant.js
@@ -0,0 +1,1 @@
+export const suggestions = ['Hawai', 'Japan', 'India', 'US', 'UAE', 'Hongkong', 'Singapore','Tokyo', 'San Francisco', 'New York', 'Mumbai','Sydney', 'Toronto'];
